2382f02f67137aaf1bcd4da7f391edd280900a16
Fox Dark Master <foxdarkmaster@hotmail.com>
2013-10-29 12:14:52 +0000

    First Version (Peleira)

---
+++ js/votos_negativos.js
@@ -0,0 +1,10 @@
+// Criar uma função que incrementar os votos negativos
+
+function votos_neg ()
+{
+  document.getElementById("vot");
+  var votos = vot.neg.value;
+  valor = votos - 1;
+  var res = value;
+  vot.neg.value=res;
+}
---
+++ js/votos_positivos.js
@@ -0,0 +1,10 @@
+// Criar uma função que incrementar os votos positivos
+
+function votos_pos ()
+{
+  document.getElementById("vot");
+  var votos = vot.pos.value;
+  valor = votos + 1;
+  var res = value;
+  vot.pos.value=res;
+}
