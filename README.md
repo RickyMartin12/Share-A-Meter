b322ec27d4caf7bee5191f589fb3b9bad57151de
Andre-Oliveira <andre.a.c.oliveira@outlook.com>
2013-11-05 12:17:31 +0000

    site V1.0
    Pronto para a apresentação

--- README.md
+++
@@ -1,5 +0,0 @@
-Share-A-Meter
-=============
-
-Web Develpment Project
-Projecto para a cadeira de Desenvolvimento de Aplicações Web do ano lectivo 2013/2014 cuja docente responsável é a professora Cristina Vieira
--- js/jquery.easing-sooper.js
+++ js/jquery.easing-sooper.js
@@ -14,7 +14,7 @@
  * 
  * Open source under the BSD License. 
  * 
- * Copyright Â© 2001 Robert Penner
+ * Copyright © 2001 Robert Penner
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
